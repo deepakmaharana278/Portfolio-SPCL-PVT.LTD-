93ecd426f18bd30b9c2ed33703ffb2b366c09cc3
Jitendra Maharana <deepakmaharana3500@gmail.com>
2026-01-31 15:48:05 +0530

    Project,contact,footer

--- scripts.js
+++ scripts.js
@@ -2,3 +2,37 @@ var typed = new Typed('#element', {
   strings: ['Python Developer', 'Fullstack Developer','Web Devloper','Coder'],
   typeSpeed: 50,
 });
+
+document.querySelectorAll("nav a").forEach(item => {
+  item.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href");
+
+    if (targetId !== "#") {
+      document.querySelector(targetId).scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
+
+const form = document.querySelector(".contact form");
+const msg = document.querySelector(".form-msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const name = form.querySelector("input[type='text']").value.trim();
+  const email = form.querySelector("input[type='email']").value.trim();
+  const message = form.querySelector("textarea").value.trim();
+
+  if (!name || !email || !message) {
+    msg.textContent = " Please fill all fields";
+    msg.style.color = "red";
+    return;
+  }
+
+  msg.textContent = "Message sent successfully!";
+  msg.style.color = "green";
+  form.reset();
+});
